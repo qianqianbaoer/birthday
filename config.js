13d3fc0056863d73545244224365be5b2263f5b7
qianqianbaoer <60434812+qianqianbaoer@users.noreply.github.com>
2020-01-29 22:29:20 +0800

    Update config.js

--- config.js
+++ config.js
@@ -34,8 +34,8 @@ var config = {
         play: "音乐",
         bannar_coming: "颜色",
         balloons_flying: "好像少点东西",
-        cake_fadein: "蛋糕？",
-        light_candle: "蜡烛？",
+        cake_fadein: "蛋糕",
+        light_candle: "蜡烛",
         wish_message: "生日快乐",
         story: "A MESSAGE FOR YOU",
     }
